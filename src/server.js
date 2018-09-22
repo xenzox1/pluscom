3be6befb549443b0a60b7f15a79fb5072b9b27fe
Enzo <xenzox9@gmail.com>
2018-09-22 16:52:44 -0300

    Changes

--- src/server.js
+++ src/server.js
@@ -1,20 +1,20 @@
 import express from 'express';
 import session from 'express-session';
-import http from 'http';
-// import https from 'https';
-// import fs from 'fs';
+// import http from 'http';
+import https from 'https';
+import fs from 'fs';
 import clientRouter from './client/index';
 import serverRouter from './server/index';
 import Io from "./server/socket/Socket";
 import config from "./server/sessionConfig";
 
-// const chavePrivada = fs.readFileSync('../https/localhost+2-key.pem', 'utf8');
-// const certificado = fs.readFileSync('../https/localhost+2.pem', 'utf8');
-// const credentials = {key: chavePrivada, cert: certificado};
+const chavePrivada = fs.readFileSync('./https/localhost+2-key.pem', 'utf8');
+const certificado = fs.readFileSync('./https/localhost+2.pem', 'utf8');
+const credentials = {key: chavePrivada, cert: certificado};
 
 const app = express();
-// const server = https.Server(credentials, app);
-const server = http.Server(app);
+const server = https.Server(credentials, app);
+// const server = http.Server(app);
 
 //Sessions
 app.use(session(config));
